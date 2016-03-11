cdbc5c5865cd4bc3f63f12122aff91fb9039fb35
fromdenisvieira <denisvieira05@gmail.com>
2016-03-11 10:51:21 -0300

    android design library - google codelab

---
+++ android-design-library/app/src/main/java/com/example/android/androiddesignlibrary/DetailActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.androiddesignlibrary;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by denisvieira on 11/03/16.
+ */
+public class DetailActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // Set Collapsing Toolbar layout to the screen
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        // Set title of Detail page
+        collapsingToolbar.setTitle(getString(R.string.item_title));
+    }
+}
