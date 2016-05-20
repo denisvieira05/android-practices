4cac45fc2b04c24e235216bc48e80c552da6a213
Denis Vieira <denisvieira05@gmail.com>
2016-05-20 16:11:57 -0300

    added obtendo-dados-de-outra-activity

---
+++ Book-Dominando-Android-2-Edicao-Nelson-Glauber/Cap-2-Activitys-e-Intents/Obtendo-dados-de-outra-activity/app/src/test/java/com/example/denisvieira/obtendo_dados_de_outra_activity/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.denisvieira.obtendo_dados_de_outra_activity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
