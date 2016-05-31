675c7aff052cb1072d24955ff5764f2829cf99ea
Denis Vieira <denisvieira05@gmail.com>
2016-05-31 11:42:37 -0300

    init app for push

---
+++ Book-Dominando-Android-2-Edicao-Nelson-Glauber/Cap-14-APIs-do-Google/Google-Cloud-Messaging/Hotel/app/src/main/java/org/js/denisvieira/hotel/GCM/HotelGcmListenerService.java
@@ -0,0 +1,51 @@
+package org.js.denisvieira.hotel.GCM;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+import org.js.denisvieira.hotel.R;
+
+/**
+ * Created by denisvieira on 31/05/16.
+ */
+public class HotelGcmListenerService extends GcmListenerService {
+    public static final int NOTIFICATION_ID = 1;
+
+    @Override
+    public void onMessageReceived(String from, Bundle data){
+        super.onMessageReceived(from,data);
+        startService(new Intent(this,HotelIntentService.class));
+        if(data != null){
+            dispararNotificacao(data.getString("mensagem"));
+        }
+    }
+
+    private void dispararNotificacao(String msg){
+        NotificationManagerCompat nm = NotificationManagerCompat.from(this);
+        Intent it = new Intent(this,HotelActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntent(it);
+        PendingIntent pit = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                .setAutoCancel(true)
+                .setContentIntent(pit)
+                .setColor(ContextCompat.getColor(this,R.color.colorAccent))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.texto_notificacao))
+                .setContentText(msg);
+        nm.notify(NOTIFICATION_ID,mBuilder.build());
+
+    }
+}
---
+++ Book-Dominando-Android-2-Edicao-Nelson-Glauber/Cap-14-APIs-do-Google/Google-Cloud-Messaging/Hotel/app/src/main/java/org/js/denisvieira/hotel/MainActivity.java
@@ -0,0 +1,16 @@
+package org.js.denisvieira.hotel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String SERVER_API_KEY = "AIzaSyAbRmVWvaq2Jx6z47CNoTyH_JXn9TimbvI";
+    public static final String SENDER_ID = "315598637492";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
